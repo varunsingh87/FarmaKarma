9a38dc6ee19490e7ebd9f630568b6bd8eaca457e
Varun <31084854+Borumer@users.noreply.github.com>
2020-05-16 16:03:33 -0400

    Insert cropdata

--- Constants.js
+++ Constants.js
@@ -1,3 +1,11 @@
 class Constants {
 	 	static get FDCKey() { return "Guvj4dILeJhRpKCR3rgFnEeim7N3ewm2DRXrohGh"};
+		static get QuickStatsKey() { return "A37194D6-E192-30F9-9E48-4E6310B7ECC6"};
+
+		static async getData(url) {
+			let response = await fetch(url);
+			response = await response.json();
+			console.log(response);
+			return response;
+		}
 }
--- script.js
+++ script.js
@@ -1,15 +1,12 @@
-console.log(Constants.FDCKey);
-async function getFoodData() {
-	const url = `https://api.nal.usda.gov/fdc/v1/foods/search?api_key=${Constants.FDCKey}`;
-	let response = await fetch(url);
-	response = await response.json();
-	console.log(response);
-	return response;
-}
-
 async function displayFoodData() {
-	const data = await getFoodData();
-
+	const data = await Constants.getData(`https://api.nal.usda.gov/fdc/v1/foods/search?api_key=${Constants.FDCKey}`);
 	document.getElementById('food-data').innerHTML = data.foods.map(el => el.foodNutrients[0].nutrientName).join(',');
 }
+
+async function displayCropData() {
+	const data = Constants.getData(`https://quickstats.nass.usda.gov/api/get_counts/?key=${Constants.QuickStatsKey}&commodity_desc=CATTLE&year__GE=2020&state_alpha=PA`);
+	document.getElementById('crop-data').innerHTML = data.count;
+}
+
 displayFoodData();
+displayCropData();
