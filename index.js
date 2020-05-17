6a2b6b7af4fbca37bbf44c211a5e324a267bb98e
Varun <31084854+Borumer@users.noreply.github.com>
2020-05-16 21:24:05 -0400

    Add more functions and loop app
    Implement
    async runFarmerApp()
    async loopApp()
    async displayData()
    async comparePesticideData()
    async retrievePesticideData()

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
+// Import all packages
 const prompt = require('prompt-sync')();
-// Importing Packages
 const axios = require('axios');
 const Constants = require('./constants.json');
 
-const url = `http://quickstats.nass.usda.gov/api/api_GET/?key=${Constants.FDCKey}&commodity_desc=CORN&year__GE=2012&state_alpha=VA&format=JSON`;
 
-// Making an API request using promises and
+// Make an API request to QuickStats API from National Agricultural Survey Service from USDA
 async function retrieveCornData() {
+	const url = `http://quickstats.nass.usda.gov/api/api_GET/?key=${Constants.FDCKey}&commodity_desc=CORN&year__GE=2012&state_alpha=VA&format=JSON`;
   response = await axios.get(url);
   state = response.data
   return state.data[0].Value;
@@ -18,16 +18,49 @@ async function compareCornData(input) {
 	return parseInt(input) / parseInt(all);
 }
 
-async function displayData(input) {
-	console.log(await compareCornData(input));
+// Make an API request to CSV file
+async function retrievePesticideData() {
+	response = await axios.get();
+	state = response.data;
+	return;
 }
-console.log("Server Started");
 
-
-const name = prompt('Enter something you would like to evaluate: ');
-switch (name) {
-	case "corn":
-		let input = prompt('Enter your average annual yield of corn: ');
-		displayData(input);
-		break;
+async function comparePesticideData(input) {
+	let all = await retrievePesticideData();
+	return parseInt(input) / parseInt(all);
 }
+
+Number.prototype.toPercent = function() {
+	return parseFloat(this).toFixed(5) * 100 + "%";
+}
+
+async function displayData(input, callbackFunc) {
+	const output = await callbackFunc(input);
+	console.log("Your usage is " + output.toPercent() + " percent of the total amount of usage.");
+}
+console.log("Server Started...");
+
+async function runFarmerApp() {
+	const name = prompt('Enter something you would like to evaluate: ');
+	let input;
+	switch (name) {
+		case "corn":
+			input = prompt('Enter your average annual yield of corn: ');
+			await displayData(input, compareCornData);
+			break;
+		case "pesticides":
+			input = prompt('Enter your average pesticide use');
+			await displayData(input, comparePesticideData);
+			break;
+	}
+}
+
+async function loopApp() {
+	await runFarmerApp();
+	const answer = prompt("Type Y to continue: ");
+	while (answer == 'Y') {
+		await runFarmerApp();
+	}
+}
+
+loopApp();
