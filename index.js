b99f8ab8c8e4319225a488ddeb542a053cec0e74
Varun <31084854+Borumer@users.noreply.github.com>
2020-05-17 10:47:15 -0400

    Refactored code to make it more DRY

--- index.js
+++ index.js
@@ -4,18 +4,18 @@ const axios = require('axios');
 const Constants = require('./constants.json');
 const CSVToJSON = require('csvtojson');
 
+async function compareData(input, callbackFunc) {
+	let all = await callbackFunc(); // Store retrieved data
+	all = all.replace(/,/g, ''); // Remove all commas in the number
+	return parseFloat(input) / parseFloat(all); // Convert to numbers and divide
+}
+
 // Make an API request to QuickStats API from National Agricultural Survey Service from USDA
 async function retrieveCornData() {
 	const url = `http://quickstats.nass.usda.gov/api/api_GET/?key=${Constants.FDCKey}&commodity_desc=CORN&year__GE=2012&state_alpha=VA&format=JSON`;
   response = await axios.get(url);
   state = response.data
   return state.data[0].Value;
-}
-
-async function compareCornData(input) {
-	let all = await retrieveCornData();
-	all = all.replace(/,/g, '');
-	return parseInt(input) / parseInt(all);
 }
 
 // Make an API request to CSV file
@@ -25,28 +25,18 @@ async function retrievePesticideData() {
   //hello test
 }
 
-async function comparePesticideData(input) {
-	let all = await retrievePesticideData();
-	return parseInt(input) / parseInt(all);
-}
-
 async function retrieveBarleyData() {
   barley = await CSVToJSON().fromFile('PlantedCrops.csv');
   barley = barley.filter(el => el.Commodity2 == 'Barley');
   return barley[0].value;
 }
 
-async function compareBarleyData(input) {
-  let all = await retrieveBarleyData();
-  return parseInt(input) / parseInt(all);
-}
-
 Number.prototype.toPercent = function() {
-	return parseFloat(this).toFixed(5) * 100 + "%";
+	return Math.round(this * 10000) / 10000 + "%";
 }
 
 async function displayData(input, callbackFunc) {
-	const output = await callbackFunc(input);
+	const output = await compareData(input, callbackFunc);
 	console.log("Your usage is " + output.toPercent() + " percent of the total amount of usage.");
 }
 console.log("Server Started...");
@@ -57,16 +47,23 @@ async function runFarmerApp() {
 	switch (name) {
 		case "corn":
 			input = prompt('Enter your average annual yield of corn: ');
-			await displayData(input, compareCornData);
+			await displayData(input, retrieveCornData);
 			break;
 		case "pesticides":
 			input = prompt('Enter your average pesticide use');
-			await displayData(input, comparePesticideData);
+			await displayData(input, retrievePesticideData);
 			break;
     case "barley":
       input1 = prompt('Enter your average barley planting-harvesting percentage difference');
-      await displayData(input, compareBarleyData);
+      await displayData(input, retrieveBarleyData);
       break;
+		default: // For user help
+			console.log("Commands:");
+			console.log("corn");
+			console.log("pesticides");
+			console.log("barley");
+			console.log("");
+			break;
 	}
 }
 
@@ -75,7 +72,7 @@ async function loopApp() {
 	let answer = prompt("Type Y to continue: ");
 	while (answer == 'Y') {
 		await runFarmerApp();
-		answer = prompt("Type Y to continue");
+		answer = prompt("Type Y to continue: ");
 	}
 }
 
